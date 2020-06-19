4cc401d08d0cb00486b97997309150e74338fc7e
harou <harou@harou-G31T-M7>
2020-06-19 13:07:42 -0300

    Adicionando o HEADER FILE forca.h

---
+++ forca.h
@@ -0,0 +1,11 @@
+#define TAMANHO_PALAVRA 20
+#define TAMANHO_ALFABETO 26
+
+void teladevitoria(); 
+void abertura();
+void chuta();
+int avaliachute(char letra);
+int desenhaforca();
+void escolhepalavra();
+void adicionapalavra();
+int adicionaerros();
